8ab7bb180f5279e938f7401d6db802851ec676ac
Franco Corgatelli <franco.corga@gmail.com>
2017-09-18 16:06:44 -0300

    trabajo

---
+++ server/models/trabajo.js
@@ -0,0 +1,34 @@
+// grab the things we need
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var Schema = mongoose.Schema;
+
+// create a schema
+var trabajoSchema = new Schema({
+
+  numeroTrabajo: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  fechaRealizacion: Date,
+  evaluacion: String,
+  observacion: String,
+  ordenServicio: {
+    type: Schema.ObjectId,
+    ref: 'Orden',
+    required: true
+  }, //reference to the associated order
+  tipoTrabajo: {
+    type: Schema.ObjectId,
+    ref: 'TipoTrabajo',
+    required: true
+  }, //reference to the associated trabajo
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Trabajo = mongoose.model('Trabajo', trabajoSchema);
+
+// make this available to our users in our Node applications
+module.exports = Trabajo;
