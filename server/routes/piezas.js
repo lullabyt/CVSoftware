cea4ba990fbc5dfb819b8513efc7d7a7a8094ddf
Unknown <fiorellapsc@gmail.com>
2017-10-30 22:36:58 -0300

    post trabajo

--- server/routes/piezas.js
+++ server/routes/piezas.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-var Pieza = require('../models/instrumento');
+var Pieza = require('../models/pieza');
 
 
 
--- server/routes/trabajos.js
+++ server/routes/trabajos.js
@@ -35,6 +35,28 @@ router.get('/fechas',(req,res) => {
     });
 });
 
+router.post('/',(req, res) => {
+console.log(req.body);
+
+  var trabajo = new Trabajo({
+    numeroTrabajo: req.body.numeroTrabajo,
+    fechaRealizacion: req.body.fechaRealizacion,
+    evaluacion: req.body.evaluacion,
+    observacion: req.body.observacion,
+    pieza: req.body.pieza,
+    ordenServicio: req.body.ordenServicio,
+    tipoTrabajo: req.body.tipoTrabajo
+  });
+
+  //una vez creada se guarda en la base de datos
+  trabajo.save().then(function() {
+    res.json(trabajo);
+
+  }, function(err) {
+    res.send("Error al crear trabajo");
+  });
+});
+
 //get todos los trabajos pertenecientes a una orden especifica, junto con sus respectivos tipos de trabajo
 router.get('/:_id', (req, res) => {
   Trabajo.find({
